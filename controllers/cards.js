471659113b0a4f55fe572e8b1018f028f5a92e18
Bogdan Mezentsev <mezentseff.bogdan@yandex.ru>
2023-06-26 18:51:21 +0300

    коммит222

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,13 +7,24 @@ const createCard = (req, res) => {
   const newCardData = req.body;
 
   return Card.create(newCardData)
-    .then((newCard) => res.status(201).send(newCard));
+    .then((newCard) => res.status(201).send(newCard))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные' });
+      }
+      return res.status(500).send({ message: 'Ошибка' });
+    });
 };
 
 const deleteCardById = (req, res) => {
   const { cardId } = req.params;
   return Card.findByIdAndDelete(cardId)
-    .then((card) => res.status(201).send(card));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.status(201).send(card);
+    });
 };
 
 const likeCard = (req, res) => {
@@ -22,7 +33,18 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.status(201).send(card));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.status(201).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные' });
+      }
+      return res.status(500).send({ message: 'Ошибка' });
+    });
 };
 
 const dislikeCard = (req, res) => {
@@ -31,7 +53,18 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
-    .then((card) => res.status(201).send(card));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      return res.status(201).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'Некорректные данные' });
+      }
+      return res.status(500).send({ message: 'Ошибка' });
+    });
 };
 
 module.exports = {
